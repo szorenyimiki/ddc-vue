6823a510108741a77f63950e47efa6a301d3c04f
fornax <lordfornax@gmail.com>
2026-06-24 09:03:21 +0200

    search input

---
+++ src/components/common/Filter.vue
@@ -0,0 +1,9 @@
+<script setup>
+  const model = defineModel()
+</script>
+
+<template>
+  <div class="d-flex">
+    <input class="form-control me-2" type="search" placeholder="Keresés..." aria-label="Search" v-model="model">
+  </div>
+</template>
